e8b4ac70462c91dfebd0183bb256d8f8f24b3099
Yilin Sun <imi415@imi.moe>
2023-02-14 21:22:03 +0800

    Initial commit.
    Signed-off-by: Yilin Sun <imi415@imi.moe>

---
+++ source/hello_world.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+
+#include "pin_mux.h"
+#include <stdbool.h>
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+    char ch;
+
+    /* Init board hardware. */
+    /* attach 12 MHz clock to USART0 (debug console) */
+    CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
+
+    BOARD_InitPins();
+    BOARD_BootClockPLL150M(); /* Rev B device can only support max core frequency to 96Mhz.
+                                Rev C device can support 150Mhz,use BOARD_BootClockPLL150M() to boot core to 150Mhz.
+                                DEVICE_ID1 register in SYSCON shows the device version.
+                                More details please refer to user manual and errata. */
+    BOARD_InitDebugConsole();
+
+    PRINTF("hello world.\r\n");
+
+    while (1)
+    {
+        ch = GETCHAR();
+        PUTCHAR(ch);
+    }
+}
